d148cefc671993e76e3c1fdb521cf6cd7927b272
Camara <101089346+maunguCamara@users.noreply.github.com>
2024-08-23 09:25:47 +0300

    Update example.c

--- example.c
+++ example.c
@@ -1,8 +1,6 @@
-// example.c
 #include "arcfour.h"
 
 #define F fflush(stdout)
-int main(void);
 
 void printbin(int8 *input, int16 size) {
     int32 i;
@@ -10,19 +8,16 @@ void printbin(int8 *input, int16 size) {
 
     assert(size > 0);
  
-    for (i=size, p=input; i; i--, p++) {    
-        if (!((i + 1) % 2)) 
+    for (i = size, p = input; i; i--, p++) {    
+        if (!((i + 1) % 2)) {
             printf(" ");
+        }
         printf("%.02x", *p);
-        
     }   
     printf("\n");
-    return;
 }
 
-
-int main(){
-
+int main() {
     Arcfour *rc4;
     int16 skey, stext;
     char *key, *from;
@@ -33,25 +28,30 @@ int main(){
     from = "Nitatoa wapi wengine?";
     stext = strlen(from);
 
+    // Encryption
     printf("Initializing encryption"); F;
-    rc4 = rc4init((int8*)key, skey);
-    printf("done\n");
+    rc4 = rc4init((int8 *)key, skey);
+    printf(" done\n");
 
     printf(" -> '%s'\n ->", from);
-    encrypted = rc4encrypt(rc4,from, stext);
+    encrypted = rc4encrypt(rc4, (int8 *)from, stext);
     printbin(encrypted, stext);
-    rc4uninit(rc4); 
+    rc4unint(rc4); 
 
-    printf("Initializing encryption"); F;
-    rc4 = rc4init((int8*)key, skey);
-    printf("done\n");
-    
-    
-    decrypted = rc4encrypt(rc4, encrypted, stext);
+    // Decryption
+    printf("Initializing decryption"); F;
+    rc4 = rc4init((int8 *)key, skey);
+    printf(" done\n");
+
+    decrypted = rc4encrypt(rc4, encrypted, stext); // rc4encrypt works for both encryption and decryption
     printf(" -> '%s'\n ->", decrypted);
-    printbin(encrypted, stext);
-    rc4uninit(rc4); 
+    printbin(decrypted, stext);
+    
+    rc4unint(rc4); 
+
+    // Free allocated memory
+    free(encrypted);
+    free(decrypted);
+
     return 0;
-
-
 }
